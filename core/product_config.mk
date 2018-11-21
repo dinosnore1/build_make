e38b1b4d328286ef5c7b4edce68c2366cd67d466
shagbag913 <sh4gbag913@gmail.com>
2018-11-21 18:25:27 -0500

    product_config: support blaze_<device>.mk
    * sailfish depends on the marlin device tree, which will both have a
    bootleggers makefile. They can't both be named bootleg.mk, so we name
    them bootleg_<device>.mk. Check if bootleg.mk exists, and if not use
    bootleg_$(BOOTLEGGERS_BUILD).mk.
    
    test: marlin & sailfish now lunches correctly.
            device trees with bootleg.mk also lunch.
    
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- core/product_config.mk
+++ core/product_config.mk
@@ -177,7 +177,11 @@ include $(BUILD_SYSTEM)/device.mk
 
 # A Blaze build needs only the specific product makefiles.
 ifneq ($(BLAZE_BUILD),)
-  all_product_configs := $(shell find device -path "*/*/blaze.mk")
+  ifneq ($(wildcard $(shell find device -path "*/*/blaze.mk")),)
+    all_product_configs := $(shell find device -path "*/*/blaze.mk")
+  else
+    all_product_configs := $(shell find device -path "*/*/blaze_$(BLAZE_BUILD).mk")
+  endif
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
